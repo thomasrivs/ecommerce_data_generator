5681ea3a45f5c0bab5c68a3431cf67f04239d263
j <j>
2024-02-18 10:19:24 +0100

    link between sales data generator and app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from src.sales import Sales
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-COUNTRIES = ["FRANCE", "UK", "GERMANY", "ITALY", "SPAIN"]
-STORE_ID = ["fr_1234", "fr5678", "uk_1234", "uk_5678", "de_1234","de_5678", "it_1234", "it_5678", "es_1234", "es_45678"]
 
+COUNTRIES = ["fr", "uk", "de", "it", "es"]
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_sales_data():
-    return {"message": "country", "data": str(Sales(2023,1,1,4).generate_ecommerce_data())}
+async def read_sales_data(country, year, month, day, hour):
+    if country in COUNTRIES:
+        return JSONResponse(content=Sales(year=year,month=month, day=day, hour=hour).generate_ecommerce_data())
