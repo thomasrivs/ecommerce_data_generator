d2572d1d8d7d64ae328f516028f3b25c0a709cb6
j <j>
2024-02-19 09:11:21 +0100

    created tests structures

---
+++ test/test_app.py
@@ -0,0 +1,10 @@
+import unittest
+
+from app import read_sales_data
+
+class TestReadSalesData(unittest.TestCase):
+    def test_read_sales_data(self):
+        #self.assertEqual(calculate_factorial(0), 1)
+
+if __name__ == '__main__':
+    unittest.main()
