f2a3b9d4cc187c0434a6192490d3a61f5c969f21
j <j>
2024-02-19 11:15:40 +0100

    added tests for app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,28 @@
 import unittest
-
-from app import read_sales_data
+from fastapi.testclient import TestClient
+from app import app
 
 class TestReadSalesData(unittest.TestCase):
-    def test_read_sales_data(self):
-        self.assertEqual(read_sales_data(0), 1)
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_read_sales_data_invalid_country(self):
+        self.assertEqual(self.client.get("/?country=nl&year=2023&month=1&day=12&hour=12").json(),  "error: Invalid country requested")
+
+    def test_read_sales_data_old_date(self):
+        self.assertEqual(self.client.get("/?country=fr&year=2018&month=1&day=12&hour=12").json(),  "error: No data before 2020")
+
+    def test_read_sales_data_future_date(self):
+        self.assertEqual(self.client.get("/?country=fr&year=2032&month=1&day=12&hour=12").json(),  "error: Please select a correct date")
+
+    def test_read_sales_data_invalid_month(self):
+        self.assertEqual(self.client.get("/?country=fr&year=2022&month=32&day=12&hour=12").json(),  "error: Please select a correct date")
+
+    def test_read_sales_data_invalid_day(self):
+        self.assertEqual(self.client.get("/?country=fr&year=2022&month=3&day=32&hour=12").json(),  "error: Please select a correct date")
+
+    def test_read_sales_data_invalid_hour(self):
+        self.assertEqual(self.client.get("/?country=fr&year=2022&month=3&day=12&hour=34").json(),  "error: Please select a correct hour")
 
 if __name__ == '__main__':
     unittest.main()
