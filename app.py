7690260f4ec8ca5188b45b6b91d34f0d6ad33527
j <j>
2024-02-19 10:42:55 +0100

    added error handling within the API

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from src.sales import Sales
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
+from datetime import datetime
+
 
 COUNTRIES = ["fr", "uk", "de", "it", "es"]
 
@@ -14,5 +16,29 @@ async def read_sales_data(country : str,
                           month : int,
                           day : int,
                           hour : int):
-    if country in COUNTRIES:
-        return JSONResponse(content=Sales(year=year,month=month, day=day, hour=hour).generate_ecommerce_data())
+
+    # Country management
+    if country not in COUNTRIES:
+        return JSONResponse(status_code=400, content=("error: Invalid country requested"))
+
+    # Year management
+    if year > datetime.today().year:
+        return JSONResponse(status_code=400, content=("error: Please select a correct date"))
+
+    elif year < 2020:
+        return JSONResponse(status_code=400, content=("error: No data before 2020"))
+
+    # Month management
+    if not 1 <= month <= 12:
+        return JSONResponse(status_code=400, content=("error: Please select a correct date"))
+
+    # Day management
+    if not 1 <= day <= 31:
+        return JSONResponse(status_code=400, content=("error: Please select a correct date"))
+
+    # Hour management
+    if not 0 <= hour <= 23:
+        return JSONResponse(status_code=400, content=("error: Please select a correct hour"))
+
+    else:
+        return JSONResponse(status_code=200, content=Sales(year=year, month=month, day=day, hour=hour).generate_ecommerce_data())
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from app import read_sales_data
 
 class TestReadSalesData(unittest.TestCase):
     def test_read_sales_data(self):
-        #self.assertEqual(calculate_factorial(0), 1)
+        self.assertEqual(read_sales_data(0), 1)
 
 if __name__ == '__main__':
     unittest.main()
